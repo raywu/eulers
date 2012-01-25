54cf32de9dec4f0eb2eb1f90fe18d5dc68110a5b
Ray Wu <raymond.cj.wu@gmail.com>
2012-01-25 00:00:10 -0500

    added problems on top

--- problem1.rb
+++ problem1.rb
@@ -1,3 +1,7 @@
+#Problem 1
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 #Use inject method on arrays
 
 a = (0...1000).to_a #array of 0 to 999
