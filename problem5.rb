39388f6e60762a11aa0fa9b1b44933cc867235a5
Ray Wu <raymond.cj.wu@gmail.com>
2012-01-30 22:06:35 -0500

    Used lcm method on integer with inject

--- problem5.rb
+++ problem5.rb
@@ -7,17 +7,17 @@ puts "The smallest evenly divsible number from 1 to 20 is #{answer}"
 
 # Iteration 2
 # This took forever
-def number(n, limit)
-  a = (2..n).inject { |result, element| result * element }
-  b = []
-  (2..limit).each { |x|
-    b << x if x % 2 == 0  && x % 3 == 0 && x % 4 == 0 && x % 5 == 0 && x % 6 == 0 && x % 7 == 0 && x % 8 == 0 && x % 9 == 0 && x % 10 == 0 #&& x % 11 == 0 && x % 12 == 0 && x % 13 == 0 && x % 14 == 0 && x % 15 == 0 && x % 16 == 0 && x % 17 == 0 && x % 18 == 0 && x % 19 == 0 && x % 20 == 0
-  }
-  b.flatten!
-  b.uniq!
-  b.min
-end
-
+# def number(n, limit)
+#   a = (2..n).inject { |result, element| result * element }
+#   b = []
+#   (2..limit).each { |x|
+#     b << x if x % 2 == 0  && x % 3 == 0 && x % 4 == 0 && x % 5 == 0 && x % 6 == 0 && x % 7 == 0 && x % 8 == 0 && x % 9 == 0 && x % 10 == 0 #&& x % 11 == 0 && x % 12 == 0 && x % 13 == 0 && x % 14 == 0 && x % 15 == 0 && x % 16 == 0 && x % 17 == 0 && x % 18 == 0 && x % 19 == 0 && x % 20 == 0
+#   }
+#   b.flatten!
+#   b.uniq!
+#   b.min
+# end
+#
 # p number(20, 1E10)
 
 
