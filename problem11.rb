03c7b8c84670e4ca90448f58af1ca9c7e7e4c000
Ray Wu <raymond.cj.wu@gmail.com>
2012-10-02 02:21:56 +1000

    complete and refactor problem 11

--- problem11.rb
+++ problem11.rb
@@ -25,46 +25,159 @@
 
 # What is the greatest product of four adjacent numbers in any direction (up, down, left, right, or diagonally) in the 2020 grid?
 
-# x, y = 0, 0
-# b = []
+grid_string = <<EOS
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+EOS
 
-# def horizontalright(x, y)
-#   b << (x, y)
-#   b << (x + 1, y + 1)
-#   b << (x + 2, y + 2)
-#   b << (x + 3, y + 3)
+grid = []
+grid_string.each_line do |line|
+  grid << line.split #=> array of rows
+  end
+
+grid.map! do |row|
+  row.map! do |x|
+    x.to_i
+  end
+end
+
+$max = 0
+
+def compute(array)
+  product = []
+  i = 0
+  for line in array
+    while line[i+3]
+      product << line[i] * line[i+1] * line[i+2] * line[i+3]
+      i += 1
+    end
+    i = 0
+  end
+  product.max #=> outputs max
+end
+
+def max?(x)
+  if $max < x
+    $max = x
+  end
+end
+
+# ==HORIZONTAL==
+
+max?(compute(grid))
+
+# --Original Code before refactor--
+# for row in grid
+#   while row[i+3]
+#     product << row[i] * row[i+1] * row[i+2] * row[i+3]
+#     i += 1
+#   end
+#   i = 0
 # end
 
-y = [
-  %w(08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08).map { |y| y.to_i },
-  %w(49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00).map { |y| y.to_i },
-  %w(81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65).map { |y| y.to_i },
-  %w(52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91).map { |y| y.to_i },
-  %w(22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80).map { |y| y.to_i },
-  %w(24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50).map { |y| y.to_i },
-  %w(32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70).map { |y| y.to_i },
-  %w(67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21).map { |y| y.to_i },
-  %w(24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72).map { |y| y.to_i },
-  %w(21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95).map { |y| y.to_i },
-  %w(78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92).map { |y| y.to_i },
-  %w(16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57).map { |y| y.to_i },
-  %w(86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58).map { |y| y.to_i },
-  %w(19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40).map { |y| y.to_i },
-  %w(04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66).map { |y| y.to_i },
-  %w(88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69).map { |y| y.to_i },
-  %w(04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36).map { |y| y.to_i },
-  %w(20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16).map { |y| y.to_i },
-  %w(20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54).map { |y| y.to_i },
-  %w(01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48).map { |y| y.to_i }
-]
+# horizontal_product = compute(grid)
+#   if max < horizontal_product
+#     max = horizontal_product
+#   end
 
-b = []
+# ==VERTICAL==
 
-y.each { |x|
-  x = 0
-  b << y[x][x] * y[x][x+1] * y[x][x+2] * y[x][x+3]
-  x += 1
-  break if y[x][x+3] == nil
-}
+num = []
+arr_col = []
 
-p b.size # => 20 for some reasons; b yields 20 instances of 34144
+grid[i].each_index do |x|
+  while grid[i]
+    num << grid[i][x]
+    i += 1
+  end
+  arr_col << num
+  i = 0
+  num = []
+end
+
+max?(compute(arr_col))
+
+# --Original Code before refactor--
+# for line in arr_col
+#   while line[i+3]
+#     product << line[i] * line[i+1] * line[i+2] * line[i+3]
+#     i += 1
+#   end
+#   i = 0
+# end
+
+# vertical_product = compute(arr_col)
+#   if max < vertical_product
+#     max = vertical_product
+#   end
+
+# ==DIAGNAL Upper Left to Bottom Right==
+
+num = []
+arr_utb = []
+
+grid.each_index do |i|
+  grid[i].each_index do |x|
+    while grid[i]
+      num << grid[i][x]
+      i += 1
+      x += 1
+    end
+    arr_utb << num.compact
+    num = []
+  end
+end
+
+max?(compute(arr_utb))
+
+# --Original Code before refactor--
+# utb_product = compute(arr_utb)
+#   if max < utb_product
+#     max = utb_product
+#   end
+
+# ==DIAGNAL Bottom Left to Upper Right==
+
+num = []
+arr_btu = []
+
+grid.each_index do |i|
+  grid[i].each_index do |x|
+    while grid[20-i]
+      num << grid[20-i][x]
+      i += 1
+      x += 1
+     break if (20 - i) < 0 # this prevents the collection to continue recursively
+    end
+    arr_btu << num.compact
+    num = []
+  end
+end
+
+max?(compute(arr_btu))
+
+# --Original Code before refactor--
+# btu_product = compute(arr_btu)
+#   if max < btu_product
+#     max = btu_product
+#   end
+  
+puts "The highest product is #{$max}"
